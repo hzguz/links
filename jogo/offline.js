a3bfbc3dcbb0d7ba9e60fbc20c6f4d0156f2bf37
Hz Guz <gxdbeats@gmail.com>
2024-12-09 14:07:26 -0300

    Add files via upload

---
+++ jogo/offline.js
@@ -0,0 +1,30 @@
+﻿{
+	"version": 1733763904,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/bird-sheet0.png",
+		"images/tiledbackground.png",
+		"images/tiledbackground2.png",
+		"images/background-sheet0.png",
+		"images/bottompipe-sheet0.png",
+		"images/toppipe-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/btnplay-sheet0.png",
+		"images/loja-sheet0.png",
+		"images/tiledbackground3.png",
+		"images/carrega-sheet0.png",
+		"images/skipar-sheet0.png",
+		"media/tapa rasta.ogg",
+		"media/sd8bit.ogg",
+		"media/ai.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
